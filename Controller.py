931363d92942965f6613408fdf0ab303de35c071
Renato de Medeiros Tranchesi <60566505+renatodmt@users.noreply.github.com>
2021-03-25 08:06:26 -0300

    Create Controller.py
    Initial commit of the controller class with the structure and what the class should do.

---
+++ Controller.py
@@ -0,0 +1,20 @@
+"""
+The controller will be responsible to get inputs from the user and run the experiment. 
+Informations such as what are the preprocessing algorithms, models, how many ML process 
+to run or the time to take into the experiment. After the experiment runs, the controller will
+resonsible to save and communicate the results.
+
+Glossary:
+Experiment -> Test different ML process to find the best for a given dataset and problem.
+ML Process -> Combination of Preprocessing Algorithms and Model to get to a prediction.
+"""
+
+class Controller:
+    def __init__():
+        pass
+    
+    def run_experiment():
+        pass
+    
+    def show_results():
+        pass
