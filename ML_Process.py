3f9cbe83a8fe6d597b7fda3802ae2fc0787f1b71
Renato de Medeiros Tranchesi <60566505+renatodmt@users.noreply.github.com>
2021-03-25 08:42:13 -0300

    Create ML_Process.py

---
+++ ML_Process.py
@@ -0,0 +1,18 @@
+'''
+This class will be responsible to receive the preprocessing algorithms and estimator from the controller,
+create a dynamic pipeline, train, predict and score the pipeline, communicate back to the controller the results.
+The ML Process saves the trained preprocessing algorithms and estimator to be used to restimate the model in the test period.
+'''
+
+class ML_Process:
+  def __init__():
+    pass
+  
+  def fit():
+    pass
+  
+  def predict():
+    pass
+  
+  def score():
+    pass
