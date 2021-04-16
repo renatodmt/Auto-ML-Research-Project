bd0681a44530f7e88c78f89ccd8eaadf525254d0
edajkram1011 <72741521+edajkram1011@users.noreply.github.com>
2021-04-16 10:32:11 +1000

    Update sample_process.py

--- sample_process.py
+++ sample_process.py
@@ -4,7 +4,22 @@ from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
 from sklearn.metrics import r2_score
 from Controller import Controller
 
+'''
+#to be inserted for reading user input json
+#import json
+#mlp_filename = "/content/ml_process.json"
+#with open (mlp_filename) as access_json:
+  #read_content = json.load(access_json)
 
+#num_of_ppm = len(read_content['Preprocessing'])
+
+#for preprocess_method in read_content['Preprocessing']:
+  #print(preprocess_method['name'])
+  #print(preprocess_method['parameter'])
+#for ml_method in read_content['MLA']:
+  #print(ml_method['name'])
+  #print(ml_method['parameter'])
+'''
 features, target = load_boston(return_X_y=True)
 vt = VarianceThreshold()
 kbest = SelectKBest()
