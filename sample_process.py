cfb1348288d11dd63d082ce58a7e1fddfa2f4cf6
Renato de Medeiros Tranchesi <renatodmt@gmail.com>
2021-04-21 18:18:15 -0300

    Added random parameters in the pipeline creator

--- sample_process.py
+++ sample_process.py
@@ -1,42 +1,19 @@
-from sklearn.feature_selection import VarianceThreshold, SelectKBest
-from sklearn.datasets import load_boston
-from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
-from sklearn.metrics import r2_score
 from Controller import Controller
 
-'''
-#to be inserted for reading user input json
-#import json
-#mlp_filename = "/content/ml_process.json"
+import json
 
-#link of json in github
-#mlp_filename = "https://github.com/renatodmt/Auto-ML-Research-Project/blob/main/ml_process.json"
-
-#with open (mlp_filename) as access_json:
-  #read_content = json.load(access_json)
-
-#num_of_ppm = len(read_content['Preprocessing'])
-
-#for preprocess_method in read_content['Preprocessing']:
-  #print(preprocess_method['name'])
-  #print(preprocess_method['parameter'])
-#for ml_method in read_content['MLA']:
-  #print(ml_method['name'])
-  #print(ml_method['parameter'])
-'''
+from sklearn.datasets import load_boston
+from sklearn.metrics import r2_score
 
 
 features, target = load_boston(return_X_y=True)
-vt = VarianceThreshold()
-kbest = SelectKBest()
-random_forest = RandomForestRegressor()
-adaboost = AdaBoostRegressor()
 score_func = r2_score
+with open('countructor.json', 'r') as fp:
+    constructor_json = json.load(fp)
 
 c = Controller(features=features,
                target=target,
                score_func=score_func,
-               pre_estimators=[vt, kbest],
-               estimators=[random_forest,adaboost],
-               pipelines_to_test=5)
-c.run_experiment()
+               pipeline_constructor_json=final_json,
+               pipelines_to_test=50)
+
